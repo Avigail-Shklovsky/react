94ac49dea1332d0dba6b4f3198d85ae391fdd242
Avigail Shklovsky <9013825@gmail.com>
2024-10-15 10:26:09 +0300

    first usage

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Header from './components/Header'; 
+import Paragraph from './components/Paragraph';
+
+function App() {
+  return (
+    <div className="App">
+     <Header></Header>
+     <Paragraph/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Paragraph.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Paragraph = () => {
+    return (
+        <h1>hello</h1>
+    )
+}
+
+export default Paragraph
