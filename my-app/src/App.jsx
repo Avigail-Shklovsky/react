ed511b25526ca6d80509152bbc9dc7beca7facda
Avigail Shklovsky <9013825@gmail.com>
2024-10-27 13:18:35 +0200

    routing

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,3 @@
-// import logo from './logo.svg';
-// import Header from './components/Header'; 
-// import Paragraph from './components/Paragraph';
-// import Link from './components/Link';
-// import Button from './components/Button';
-// import Image from './components/Image';
-
 import Header from "./components/Header";
 import { ItemList } from "./components/ItemList";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
